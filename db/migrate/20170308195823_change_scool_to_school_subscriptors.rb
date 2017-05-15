af2ff95bebf96d01635a6a7530c74c517a431895
polken <genesis62@hotmail.com>
2017-05-15 19:52:00 +0000

    fix 4  heroku

--- db/migrate/20170308195823_change_scool_to_school_subscriptors.rb
+++ db/migrate/20170308195823_change_scool_to_school_subscriptors.rb
@@ -1,6 +1,6 @@
 class ChangeScoolToSchoolSubscriptors < ActiveRecord::Migration
   def change
-    rename_column :subscriptors, :scool_id, :school_id
+    #rename_column :subscriptors, :scool_id, :school_id
     #remove_index :subscriptors,column: 'scool_id'
     #rename_index :subscriptors, :index_subscriptors_on_scool_id, :index_subscriptors_on_school_id
   end
